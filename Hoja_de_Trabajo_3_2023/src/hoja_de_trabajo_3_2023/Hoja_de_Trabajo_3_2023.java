230f0819ac2f5ea2f911bbef57c77aa38eb7a788
Yo pero en la U <3875424500101@ingenieria.usac.edu.gt>
2023-04-14 13:21:46 -0600

    Programa Java
    Archivo de Java con el programa de las áreas de las figuras.

---
+++ Hoja_de_Trabajo_3_2023/src/hoja_de_trabajo_3_2023/Hoja_de_Trabajo_3_2023.java
@@ -0,0 +1,19 @@
+package hoja_de_trabajo_3_2023;
+import java.util.Scanner;
+
+public class Hoja_de_Trabajo_3_2023 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Funciones fn = new Funciones();
+        
+        int op;
+        
+        do {
+            fn.menu();
+            op=sc.nextInt();
+            fn.menu_opciones(op);
+        } while(op!=5);
+    }
+    
+}
