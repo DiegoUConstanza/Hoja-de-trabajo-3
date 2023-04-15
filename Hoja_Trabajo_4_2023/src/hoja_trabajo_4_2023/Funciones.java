c192554426fa80465380ee01adf1ca6c5fa7b991
Yo pero en la U <3875424500101@ingenieria.usac.edu.gt>
2023-04-14 21:56:59 -0600

    Subida del Proyecto correcto
    Esperemos que esta vez funcione.

---
+++ Hoja_Trabajo_4_2023/src/hoja_trabajo_4_2023/Funciones.java
@@ -0,0 +1,82 @@
+package hoja_trabajo_4_2023; 
+import java.util.Scanner;
+
+public class Funciones {
+    
+    Scanner sc = new Scanner (System.in);
+    
+    public int op;
+    public double radio,volumen,area,perimetro;
+    public double base,altura,largo;
+    Figuras fg = new Figuras();
+    
+    //Funciones
+    
+    int menu_opciones(int op){
+        
+        switch(op){
+            case 1:
+                System.out.println("_____Has elegido el area de la esfera_____");
+                System.out.println("Por favor ingrese el valor del radio");
+                radio=sc.nextDouble();
+                fg.esfera(radio, volumen, area, perimetro);
+            break;
+            
+            case 2:
+                System.out.println("_____Has elegido el area del paralelepipedo_____");
+                System.out.println("Por favor ingrese el valor de la base");
+                base=sc.nextDouble();
+                System.out.println("Por favor ingrese el valor del largo");
+                largo=sc.nextDouble();
+                System.out.println("Por favor ingrese el valor de la altura");
+                altura=sc.nextDouble();
+                fg.paralelepipedo(base, largo, altura, volumen, area, perimetro);
+            break;
+            
+            case 3:
+                System.out.println("_____Has elegido el area del cono_____");
+                System.out.println("Por favor ingrese el valor del radio");
+                radio=sc.nextDouble();
+                System.out.println("Por favor ingrese el valor de la altura");
+                altura=sc.nextDouble();
+                fg.cono(radio,altura,volumen,perimetro,area);
+            break;
+            
+            case 4:
+                System.out.println("_____Has elegido el area del cilindro_____");
+                System.out.println("Por favor ingrese el valor del radio");
+                radio=sc.nextDouble();
+                System.out.println("Por favor ingrese el valor de la altura");
+                altura=sc.nextDouble();
+                fg.cilindro(radio,altura,volumen,perimetro,area);
+            break;
+            
+            case 5:
+                System.out.println("Nos vemos pronto :D!");
+            break;
+            
+            default: 
+                System.out.println("Por favor seleccione una opcion valida");
+            break;
+        }
+        return 0;
+    }
+           
+    //Metodos
+    public void menu(){
+        System.out.println("__________Menu de figuras tridimensionales__________");
+        System.out.println("1.Esfera");
+        System.out.println("2.Paralelepipedo");
+        System.out.println("3.Cono");
+        System.out.println("4.Cilindro");
+        System.out.println("5.Salir");
+        System.out.println("Ingresa una opcion");
+    }
+    
+    //No funciona :c
+    /*public void limpiarconsola()
+        System.out.print("\033[H\033[2J");  
+        System.out.flush();  
+    }1*/
+    
+}
